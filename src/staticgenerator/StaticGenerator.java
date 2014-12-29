6d095ddb21526f7f5a5bcc3ce5ea325b0282ab74
Matthew Ewer <Ewer.Matthew@gmail.com>
2014-12-29 02:50:27 -0700

    New project: generate static, for example to draw on it those weird images your brain pulls out of it.

---
+++ src/staticgenerator/StaticGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staticgenerator;
+
+/**
+ *
+ * @author MEwer
+ */
+public class StaticGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SGFrame frame = new SGFrame();
+        frame.setVisible(true);
+    }
+}
